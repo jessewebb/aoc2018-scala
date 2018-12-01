33a12a2f75a498ab22bec3f7d0e0410f57259562
Jesse Webb <gweebz@gmail.com>
2018-12-01 01:39:16 -0600

    Add main function to run Solutions

--- src/main/scala/jessewebb/aoc2018/Program.scala
+++ src/main/scala/jessewebb/aoc2018/Program.scala
@@ -1,7 +1,23 @@
 package jessewebb.aoc2018
 
+import scala.io.Source
+
 object Program extends App {
 
-  println("Hello world!")
+  def solveDay01(): Unit = {
+    println("Day 01\n======")
+    import jessewebb.aoc2018.day01._
+    val inputResource = "inputs/day01.txt"
+    val inputStream = getClass.getClassLoader.getResourceAsStream(inputResource)
+    val input = Source.fromInputStream(inputStream).mkString
+    val frequencies = input.split('\n').map(_.trim).toList
+    val solution1 = Part1.solve(frequencies)
+    println(s"Part 1: $solution1")
+    val solution2 = Part2.solve(frequencies)
+    println(s"Part 2: $solution2")
+    println()
+  }
+
+  solveDay01()
 
 }
